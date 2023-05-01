332b211033d06daaa7796300d6ca8b30bd356ec0
JAE SKEETE <127004078+skeetej@users.noreply.github.com>
2023-05-01 14:46:25 -0400

    UPDATE

--- Assets/SCRIPTS - JAE/SCRIPTS/PlayerController.cs
+++ Assets/SCRIPTS - JAE/SCRIPTS/PlayerController.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private float _speed = 5; 
+    [SerializeField] private float _turnSpeed = 360;
+  private Vector3 _input;
 
-    // Update is called once per frame
-    void Update()
+
+  void Update ()
+  {
+    GatherInput();
+    Look();
+  }
+
+  void FixedUpdate()
+  {
+    Move();
+  }
+
+
+  void GatherInput()
+  {
+    _input = new Vector3(Input.GetAxisRaw("Vertical"), 0, Input.GetAxisRaw("Horizontal"));
+  }
+
+  void Look()
+  {
+    if (_input != Vector3.zero)
     {
-        
+
+        //var matrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
+
+        //var skewedInput = matrix.MultiplyPoint3x4(_input);
+
+        //_input.ToIso()
+
+        var relative = (transform.position + _input) - transform.position;
+        var rot = Quaternion.LookRotation(relative, Vector3.up);
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, _turnSpeed * Time.deltaTime);
     }
+  }
+
+  void Move()
+  {
+    _rb.MovePosition(transform.position + (transform.forward * _input.magnitude) * _speed * Time.deltaTime);
+  }
 }
---
+++ Assets/SCRIPTS - JAE/SCRIPTS/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private float _speed = 5; 
+    [SerializeField] private float _turnSpeed = 360;
+
+    private InputControlls input = null;
+    private Vector3 moveVector = Vector3.zero;
+
+    private void Awake()
+    {
+        input = new InputControlls();
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        input.Enable();
+        input.Player.Movement.performed += OnMovementPreformed;
+        input.Player.Movement.canceled += OnMovementCanceled;
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+        input.Player.Movement.performed -= OnMovementPreformed;
+        input.Player.Movement.canceled -= OnMovementCanceled;
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = moveVector * _speed;
+    }
+
+    private void OnMovementPreformed(InputAction.CallbackContext value)
+    {
+        moveVector = value.ReadValue<Vector3>();
+    }
+
+    
+
+    private void OnMovementCanceled(InputAction.CallbackContext value)
+    {
+        moveVector = Vector3.zero;
+    }
+}
